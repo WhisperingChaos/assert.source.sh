3ab90289d32217b4ca4e515f27bf31dbb56bad2d
Rich Moyse <rich@moyse.us>
2017-11-17 18:24:17 -0500

    added cautionary notes, flipped notion of true/false, assert output functions allows specification of regex/equality comparison at the granularity of a line, introduced proper quoting of LHS/RHS for regex/equality operators

--- component/assert.include.sh
+++ component/assert.include.sh
@@ -1,58 +1,96 @@
 #!/bin/bash
+# NOTES
+#	> Variables declared within the functions below are prefixed by 'asrt'.
+#	This prefix acts as a namespace to avoid accidentially colliding with
+#	variables that are passed to the assert functions.
+#	> Function names with "assert__"... are considered private to this include
+#	file - they should only be called by functions within it.
+
+assert_REGEX_COMPARE='<RegEx>'
+
+# assert_output functions accept only a single argument.  That argument, in most
+# cases should be the name of a bash function that writes to sysout.  
 assert_output_true(){
-	assert__output_bool "$1" '!'
-}
-assert_output_false(){ 
 	assert__output_bool "$1" ' '
 }
+assert_output_false(){ 
+	assert__output_bool "$1" '!'
+}
 assert__output_bool(){
-	local -r expectedFn="$1"
-	local -r negate="$2"
+	local -r asrtExpectedFn="$1"
+	# execute expected function before defining many other
+	# local variables to ensure generation function binds,
+    # if it needs to, to variables declared outside the
+	# scope of this function. 
+	local asrtFDesc
+	exec {asrtFDesc}< <( $asrtExpectedFn ) 
+	local -r asrtNegate="$2"
 
-	local generated
-	local generatedCnt=0
-	local expected
-	local expectedCnt=0
-	local fDesc
-	exec {fDesc}< <( $expectedFn ) 
-	while read -r generated; do
-		((generatedCnt++))
-		if read -r -u $fDesc expected; then
-			((expectedCnt++))
+	local asrtCompOper
+	local asrtEval
+	local asrtGenerated
+	local asrtGeneratedCnt=0
+	local asrtExpected
+	local asrtExpectedCnt=0
+	while read -r asrtGenerated; do
+		((asrtGeneratedCnt++))
+		if read -r -u $asrtFDesc asrtExpected; then
+			((asrtExpectedCnt++))
 		fi
-		if eval $negate \[\[ \$generated \=\~ \$expected \]\]; then 
-			eval exec $fDesc\<\&\-
-			echo "generated: '$generated'">&2
-			echo "expected:  '$expected'" >&2	
+		asrtCompOper='true'
+		if [[ "${asrtExpected:0:${#assert_REGEX_COMPARE}}" == "$assert_REGEX_COMPARE" ]]; then
+			asrtCompOper='false'
+			asrtExpected="${asrtExpected:${#assert_REGEX_COMPARE}}"
+		fi
+		asrtEval='true'
+		# need two different comparators statements: 
+		#	1. Regex expressions, that appear in RHS of regex comparison,
+	   	#	must not be encapsulated in quotes because if they are quoted,
+		#	they are evaluated as just an ordnary string of characters.    
+	    #	2. The equality operator requires both its operands encapsulated
+	   	#	by double quotes to prevent certain characters like '[' and ']'
+		#	from interferring with its evaluation.
+		#	3. Finally, the LHS operand of a regex operator must also be
+		#	quote encapulated for the very same reason the equality operator's
+	   	#	LHS is encapsulted by quotes.
+		if $asrtCompOper; then 
+			eval $asrtNegate \[ \"\$asrtGenerated\" == \"\$asrtExpected\" \] \|\| asrtEval\=\'false\'
+		else
+			eval $asrtNegate \[\[ \"\$asrtGenerated\" =~ \$asrtExpected \]\] \|\| asrtEval\=\'false\'
+		 fi
+		 if ! $asrtEval; then
+			eval exec $asrtFDesc\<\&\-
+			echo "generated: '$asrtGenerated'">&2
+			echo "expected:  '$asrtExpected'" >&2	
 			return 1
 		fi
 	done
-	while read -r -u $fDesc; do
-		((expectedCnt++))
+	while read -r -u $asrtFDesc; do
+		((asrtExpectedCnt++))
 	done
-	eval exec $fDesc\>\&\- 
-	if [ $generatedCnt -ne $expectedCnt ]; then 
-		echo "generated lines $generatedCnt != $expectedCnt expected lines" >&2
+	eval exec $asrtFDesc\>\&\- 
+	if [ $asrtGeneratedCnt -ne $asrtExpectedCnt ]; then 
+		echo "generated lines $asrtGeneratedCnt != $asrtExpectedCnt expected lines" >&2
 		return 1
 	fi
 	true
 }
 assert_true(){
-	assert__bool "$1" '!'
-}
-
-assert_false(){
 	assert__bool "$1" ' '
 }
 
-assert__bool(){
-	local -r expression="$1"
-	local -r negate="$2"
+assert_false(){
+	assert__bool "$1" '!'
+}
 
-	if eval $negate $expression; then
-		# indirectly called from failing test :: use [2] to identify it.  
-		echo "status='${FUNCNAME[1]} failed' func='${FUNCNAME[2]}' lineno=${BASH_LINENO[2]} source='${BASH_SOURCE[2]}'" >&2
-		return 1
+assert__bool(){
+	local -r asrtExpression="$1"
+	local -r asrtNegate="$2"
+
+	if eval $asrtNegate $asrtExpression; then
+		return
 	fi
-	return 0
+	# indirectly called from failing test :: use [2] to identify it.  
+	echo "status='${FUNCNAME[1]} failed' func='${FUNCNAME[2]}' lineno=${BASH_LINENO[1]} source='${BASH_SOURCE[2]}'" >&2
+	return 1
 }
