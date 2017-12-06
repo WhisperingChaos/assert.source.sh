44532154af12ef94a5b65ab269287b115501bb86
Rich Moyse <rich@moyse.us>
2017-12-06 16:21:48 -0500

    document instances where assert__bool can fail, DRY assert messaging, make global variable local using double underscore convention

--- component/assert.include.sh
+++ component/assert.include.sh
@@ -3,9 +3,13 @@
 #	> Variables declared within the functions below are prefixed by 'asrt'.
 #	This prefix acts as a namespace to avoid accidentially colliding with
 #	variables that are passed to the assert functions.
-#	> Function names with "assert__"... are considered private to this include
-#	file - they should only be called by functions within it.
+#	> Function & global variable names prefixed by "assert__"... are considered
+#	private to this include	file - they should only be used/called by functions
+#	within it.
 
+# use this public constant variable to prefix the lines in the expected test
+# output to perform a regex comparison when comparing it to the generated output
+# instead of simple equality.
 assert_REGEX_COMPARE='<RegEx>'
 # assert_output functions accept only a single argument.  That argument, in most
 # cases should be the name of a bash function that writes to sysout.  
@@ -56,15 +60,10 @@ assert__output_bool(){
 			eval $asrtNegate \[ \"\$asrtGenerated\" == \"\$asrtExpected\" \] \|\| asrtEval\=\'false\'
 		else
 			eval $asrtNegate \[\[ \"\$asrtGenerated\" =~ \$asrtExpected \]\] \|\| asrtEval\=\'false\'
-		 fi
-		 if ! $asrtEval; then
+		fi
+		if ! $asrtEval; then
 			eval exec $asrtFDesc\<\&\-
-			echo "msg='${FUNCNAME[1]} failed'" >&2
-			echo "  generated='$asrtGenerated'">&2
-			echo "  expected_='$asrtExpected'" >&2	
-			echo "  lineNo=${BASH_LINENO[1]}"  >&2
-			# indirectly called from failing test :: use [2] to identify it.
-			echo "  source='${BASH_SOURCE[2]}' func='${FUNCNAME[2]}'" >&2
+			assert__msg_failed "generated='$asrtGenerated'" "expected_='$asrtExpected'"	
 			# note when participating in a pipe, recording and halting
 			# don't affect the parent process
 			assert__raised_record
@@ -78,11 +77,7 @@ assert__output_bool(){
 	eval exec $asrtFDesc\>\&\- 
 	if [ $asrtGeneratedCnt -ne $asrtExpectedCnt ]; then 
 		echo "msg='${FUNCNAME[1]} failed'" >&2
-		echo "  generatedCnt='$asrtGeneratedCnt'">&2
-		echo "  expected_Cnt='$asrtExpectedCnt'" >&2	
-		echo "  lineNo=${BASH_LINENO[1]}"  >&2
-		# indirectly called from failing test :: use [2] to identify it.
-		echo "  source='${BASH_SOURCE[2]}' func='${FUNCNAME[2]}'" >&2
+		assert__msg_failed "generatedCnt='$asrtGeneratedCnt'" "expected_Cnt='$asrtExpectedCnt'"	
 		# note when participating in a pipe, recording and halting
 		# don't affect the parent process
 		assert__raised_record
@@ -104,17 +99,40 @@ assert__bool(){
 	if eval $asrtNegate $asrtExpression; then
 		return
 	fi
-	echo "msg='${FUNCNAME[1]} failed'" >&2
-	echo "  expression=$asrtNegate $asrtExpression" >&2
-	eval echo \' \ evalExpres=\'\"\$asrtNegate\ \"\"$asrtExpression\" >&2
-	echo "  lineNo=${BASH_LINENO[1]}" >&2
-	# indirectly called from failing test :: use [2] to identify it.
-	echo "  source='${BASH_SOURCE[2]}' func='${FUNCNAME[2]}'" >&2
+	# messaging will fail if asrtExpression contains an unbalanced number of
+   	# double quotes and the odd one isn't excaped - \" .  it may also succeed
+	# but may be missing double quotes as the added enclosing ones below 
+	# trigger string concatenation.  This is usually not an issue when the
+	# asrtExpression variable encapsulates all strings in variables, as bash will properly
+	# escape all quotes using either concatenation or '\'.
+	# Ex:
+	#	b='"'
+	#   asrtExpression='$b'
+	#	eval echo \"$asrtExpression\"
+	# the expression won't trigger "unexpected EOF while looking for matching"
+	# however, the following will trigger this exception:
+	# Ex
+	#	b='"'
+	#	eval echo \"$b\"
+	# when the exception occurs, it's better to rewrite the test and encapsulate
+	# the argument within a variable as other corrective mechanisms, like capturing
+   	#	set -x or using a trap don't work very well and diminish performance.
+	#
+	eval assert__msg_failed \"expression\=\$asrtNegate \$asrtExpression\" \
+		\'evalExpres\=\'\"\$asrtNegate\ \"\"$asrtExpression\"
 	assert__raised_record
 	assert__halt_check
 }
+assert__msg_failed(){
+	echo "msg='${FUNCNAME[2]} failed'" >&2
+	echo "  $1" >&2
+	echo "  $2" >&2
+	echo "  lineNo=${BASH_LINENO[2]}" >&2
+	# indirectly called from failing test :: use [3] to identify it.
+	echo "  source='${BASH_SOURCE[3]}' func='${FUNCNAME[3]}'" >&2
+}
 # default implementation supporting asserts that immediately halt or continue
-assert_RAISED_SOMETIME_DURING_EXECUTION='false' 
+assert__RAISED_SOMETIME_DURING_EXECUTION='false' 
 assert_halt(){
 	assert__halt_check(){
 		exit 1
@@ -126,12 +144,11 @@ assert_continue(){
 	}
 }
 assert__raised_record(){
-	assert_RAISED_SOMETIME_DURING_EXECUTION='true'
+	assert__RAISED_SOMETIME_DURING_EXECUTION='true'
 }
 assert__halt_check(){
 	return 1
 }
 assert_raised_check(){
-	! $assert_RAISED_SOMETIME_DURING_EXECUTION
+	! $assert__RAISED_SOMETIME_DURING_EXECUTION
 }
-
